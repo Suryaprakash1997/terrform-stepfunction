52200142d921ebcd65ff5651e41b026dbb903160
Suryaprakash1997 <157590817+Suryaprakash1997@users.noreply.github.com>
2024-03-08 15:08:13 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-terraform init
-terraform apply -auto-approve
-if [ $? -eq 0 ]; then
-    echo "success" > script-status.txt
-else
-    echo "failure" > script-status.txt
-fi
-aws s3 cp script-status.txt s3://terrafornstatus/script-status.txt
